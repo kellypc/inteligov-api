4d62002f3cff0ea3a45ad0664f2e1dccbe8f2781
kelly <kellycruz.ti@gmail.com>
2019-03-12 22:01:41 -0300

    Add relationship between user and bill

---
+++ db/migrate/20190313005502_add_user_to_bill.rb
@@ -0,0 +1,5 @@
+class AddUserToBill < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :bills, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_121521) do
+ActiveRecord::Schema.define(version: 2019_03_13_005502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2019_03_12_121521) do
     t.string "introduction_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_bills_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -59,4 +61,5 @@ ActiveRecord::Schema.define(version: 2019_03_12_121521) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "bills", "users"
 end
